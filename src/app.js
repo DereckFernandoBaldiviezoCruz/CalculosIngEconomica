8de070a3065d397c262c67c0ad282eedc39f30fe
Dereck <dereck120120120@gmail.com>
2024-11-21 14:39:08 -0400

    Version 1.0

--- src/app.js
+++ src/app.js
@@ -10,5 +10,10 @@ app.use(express.json());
 
 // Rutas de la API
 app.use('/api', routes);
+const PORT = process.env.PORT || 3000; // Usa el puerto de Render o el puerto 3000 como predeterminado
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 
 module.exports = app;
